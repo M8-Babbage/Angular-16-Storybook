22303c25b1fc9fa95f79d274795ade9d23f3a2fc
Edwin J. Páez <edwinjpr11@gmail.com>
2023-05-10 10:33:25 -0500

    feat: update stories

--- src/app/atoms/button/button.stories.ts
+++ src/app/atoms/button/button.stories.ts
@@ -1,11 +1,35 @@
 import type { Meta, StoryObj } from '@storybook/angular'
 import { ButtonComponent } from "./button.component"
+import ButtonDocumentation from "./button.md"
 
 const meta: Meta<ButtonComponent> = {
   title: 'Design System/Atoms/Button',
   component: ButtonComponent,
-  args: {
-    label: 'Button',
+  // En este punto podemos definir los argumentos que serán requeridos en el componente
+  argTypes: {
+    label: {
+      name: 'label',
+      description: 'Texto del botón',
+      control: {
+        type: 'text',
+      },
+      required: true,
+    },
+    type: {
+      name: 'type',
+      description: 'Tipo de botón',
+      options: ['primary', 'secondary', 'tertiary'],
+      control: { type: 'radio' },
+    }
+  },
+  // Aquí podemos documentar el componente
+  parameters: {
+    docs: {
+      page: null,
+      description: {
+        component: ButtonDocumentation,
+      }
+    }
   }
 }
 
@@ -13,20 +37,9 @@ export default meta
 
 type Story = StoryObj<ButtonComponent>
 
-export const Primary: Story = {
-  args: {
-    type: 'primary',
-  }
-}
-
-export const Secondary: Story = {
-  args: {
-    type: 'secondary',
-  }
-}
-
-export const Tertiary: Story = {
-  args: {
-    type: 'tertiary',
-  }
+/**
+ * Ejemplo de una historia, debe existir al menos una, donde se podrán pasar sus argumentos por medio de la propiedad args: { type: 'primary', label: 'Button' }, en este caso no se le envía porque lo estamos controlando desde el panel de controles
+ */
+export const Default: Story = {
+  args: { label: 'Button', type: 'primary' },
 }
